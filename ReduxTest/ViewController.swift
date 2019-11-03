6346ce5725dbf21b4d8f068e66f077896b7a5511
Leonid Nifantyev <leonif1@gmail.com>
2019-11-03 12:52:01 +0200

    Initial Commit

---
+++ ReduxTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReduxTest
+//
+//  Created by Leonid Nifantyev on 11/3/19.
+//  Copyright © 2019 Lion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
